4421e746b10afb3663c80a91d5aa6e06fbd65001
Andrelise <andrelisenunes@hotmail.com>
2019-07-08 15:42:13 -0300

    Probleminhas

--- Analisador/src/analisador/parser.java
+++ Analisador/src/analisador/parser.java
@@ -12,4 +12,49 @@ package analisador;
  */
 public class parser {
     
+    public float transicao;
+    public listaDeTokens pilha;
+    public listaDeTokens entrada;
+    
+    public parser(listaDeTokens token) {
+    
+        
+        
+    }
+    
+    public void identificar(listaDeTokens input){
+        
+        //esse switch reconhece a entrada que no nosso caso é uma lista de tokens com todos do arq TXT, e eles estão classificados conforme a descrição
+        //do projeto
+        switch(input.getRotulo()){
+            //TRANSIÇÕES DO TIPO 1 - EMPILHA UM SIMBOLO NÃO TERMINAL NA PILHA 
+            /* (p, a, E, p, a)
+               (p, 1, E, p, 1)
+                p, (, E, p, (
+                p, ), E, p, )
+                p,+, E, p, +
+                p, -, E, p, -
+                p, *, E, p, *
+                p, /, E, p ,/
+                p, gt,E, p, gt
+                p, lt, E, p lt
+                p, geq, E, p, geq
+                p, leq, E, p, leq
+                p, lq, E, p, lq
+                p, neq, E, p, neq
+                p, not, E, p, not
+                p, and, E, p, and
+                p, or, E, p, or
+                p, defun, E, p, defun
+                p, if, E, p, if*/
+                
+            case DelimEsq://parentese esquerdo (
+                
+                break;
+            
+            
+        }
+        
+    }
+    
 }
